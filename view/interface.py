cf745346384e975422e3199f7aae9a482034ecd4
Wendell <wendellcs131@gmail.com>
2025-11-29 16:12:55 -0300

    Change render method

--- view/interface.py
+++ view/interface.py
@@ -11,16 +11,33 @@ from tkinter import *
 root = Tk()
 root.geometry('500x700')
 root.title('Virtual Market')
-root.grid()
+
+container = Frame(root)
+container.grid(row=0, column=0, sticky='nsew')
+
+root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
-def clear_screen(frame):
-    frame.destroy()
+container.grid_rowconfigure(0, weight=1)
+container.grid_columnconfigure(0, weight=1)
 
-def login_page():
-    login_frame = Frame(root)
-    login_frame.grid(row=0, column=0, pady=100)
+register_frame = Frame(container)
+login_frame = Frame(container)
 
+frames = [login_frame, register_frame]
+
+for frame in frames:
+    frame.grid(row=0, column=0, sticky='nsew')
+
+    frame.grid_columnconfigure(0, weight=1)
+
+login_frame.grid(row=0, column=0, pady=20)
+register_frame.grid(row=0, column=0, pady=20)
+
+def show_screen(frame):
+    frame.tkraise()
+
+def build_login():
     title = Label(login_frame, text='Faça seu login', font=('Arial', 32, 'bold'))
     title.grid(pady=10, row=0, column=0)
 
@@ -51,12 +68,74 @@ def login_page():
     label_3 = Label(box_3, text='Não possui uma conta?')
     label_3.grid(row=0, column=0, pady=(10, 0))
 
-    button_2 = Button(box_3, text='Criar uma conta', bd=0, highlightthickness=0, cursor="hand2")
+    button_2 = Button(box_3, text='Criar uma conta', bd=0, highlightthickness=0, cursor="hand2", command=lambda:(register_view()))
     button_2.grid(row=1, column=0, pady=15, ipadx=30, ipady=3)
+
+def build_register():
+    
+    title = Label(register_frame, text='Faça seu cadastro', font=('Arial', 32, 'bold'))
+    title.grid(pady=10, row=0, column=0)
+
+    box_1 = Frame(register_frame)
+    box_1.grid(row=1, column=0)
+
+    label_1 = Label(box_1, text='Nome:')
+    label_1.grid(row=0, column=0)
+
+    entry_1 = Entry(box_1)
+    entry_1.grid(row=1, column=0, ipadx=10, ipady=3)
+
+    box_2 = Frame(register_frame)
+    box_2.grid(row=2, column=0)
+
+    label_2 = Label(box_2, text='Nome de usuário:')
+    label_2.grid(row=0, column=0)
+
+    entry_2 = Entry(box_2)
+    entry_2.grid(row=1, column=0, ipadx=10, ipady=3)
+
+    box_3 = Frame(register_frame)
+    box_3.grid(row=3, column=0)
+
+    label_3 = Label(box_3, text='Senha:')
+    label_3.grid(row=0, column=0, pady=(10, 0))
+
+    entry_3 = Entry(box_3)
+    entry_3.grid(row=1, column=0, ipadx=10, ipady=3)
+
+    box_4 = Frame(register_frame)
+    box_4.grid(row=4, column=0)
+
+    label_4 = Label(box_4, text='Confirme sua senha:')
+    label_4.grid(row=0, column=0, pady=(10, 0))
+
+    entry_4 = Entry(box_4)
+    entry_4.grid(row=1, column=0, ipadx=10, ipady=3)
+
+    button_1 = Button(register_frame, text='Criar conta', cursor="hand2")
+    button_1.grid(row=5, column=0, pady=15, ipadx=30, ipady=3)
+
+    box_5 = Frame(register_frame)
+    box_5.grid(row=6, column=0)
+
+    label_4 = Label(box_5, text='Já possui uma conta?')
+    label_4.grid(row=0, column=0, pady=(10, 0))
+
+    button_2 = Button(box_5, text='Fazer login', bd=0, highlightthickness=0, cursor="hand2", command=lambda:login_view())
+    button_2.grid(row=1, column=0, pady=15, ipadx=30, ipady=3)
+
+def register_view():
+    show_screen(register_frame)
+
+def login_view():
+    show_screen(login_frame)
 
 
 def start_app():
-    login_page()
+    build_login()
+    build_register()
+
+    login_view()
     root.mainloop()
 
 start_app()
